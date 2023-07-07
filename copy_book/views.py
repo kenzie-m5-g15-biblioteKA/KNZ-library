ebcbb271878f12799217e3a9248f16f7bdc5bf37
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 13:03:07 -0300

    feat:add copy_book views

--- copy_book/views.py
+++ copy_book/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from books.models import Book
+from .models import Copy
+from .serializers import CopySerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.permissions import IsStaff, IsStaffOrSafeMethods
 
-# Create your views here.
+
+class CopyView(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsStaff]
+
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+
+class CreateCopyView(generics.CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsStaff]
+
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+    lookup_url_kwarg = "pk"
+
+    def perform_create(self, serializer):
+        self.check_object_permissions(self.request, self.request.user)
+        book = get_object_or_404(Book, pk=self.kwargs.get("pk"))
+        serializer.save(book=book)
+
+
+class CopyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsStaffOrSafeMethods]
+
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
