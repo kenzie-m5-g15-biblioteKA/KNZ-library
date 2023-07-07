b90ee9c528fd42a1428aa5cb081e7854d94a5a1e
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:57:12 -0300

    feat:add lending models

---
+++ lending/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Lending(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateField()
+    returned_date = models.DateField(null=True)
+
+    user = models.ForeignKey(
+        "users.User",
+        related_name="lending",
+        on_delete=models.CASCADE,
+    )
+
+    copy = models.ForeignKey(
+        "copy_book.Copy",
+        related_name="lending",
+        on_delete=models.CASCADE,
+    )
