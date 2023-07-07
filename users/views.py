a716792c78ffabfd8c58dd8a4e460f7734deb743
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:52:55 -0300

    feat:add user views

--- users/views.py
+++ users/views.py
@@ -1,46 +1,41 @@
+from lending.serializers import LendingsSerializer
 from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.response import Response
-from .serializers import UserSerializer
+from lending.models import Lending
+from .serializers import UserSerializer, UserStatusSerializer
 from .permissions import IsAccountOwner, IsStaff
 from rest_framework import generics
 
 
-# class UserView(generics.ListCreateAPIView):
-#     authentication_classes = [JWTAuthentication]
-#     permission_classes = [IsStaff]
-
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
-#     def list(self, request, *args, **kwargs):
-#         self.check_object_permissions(request, request.user)
-#         queryset = self.filter_queryset(self.get_queryset())
-
-#         page = self.paginate_queryset(queryset)
-#         if page is not None:
-#             serializer = self.get_serializer(page, many=True)
-#             return self.get_paginated_response(serializer.data)
-
-#         serializer = self.get_serializer(queryset, many=True)
-#         return Response(serializer.data)
-
-
-"""Teste"""
-
-
 class UserView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsStaff]
+
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = UserStatusSerializer
+
+    def get(self, request, *args, **kwargs):
+        self.check_object_permissions(request, request.user)
+        return self.list(request, *args, **kwargs)
 
 
 class UserLendingView(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    queryset = Lending.objects.all()
+    serializer_class = LendingsSerializer
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsStaff | IsAccountOwner]
 
 
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
+
+
+class UserDeleteView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsStaff]
