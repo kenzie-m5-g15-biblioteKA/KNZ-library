04aa876022b42f16f71337c0687943dcc1b0de85
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:51:06 -0300

    feat:add user permissions

--- users/permissions.py
+++ users/permissions.py
@@ -1,19 +1,29 @@
 from rest_framework import permissions
 from .models import User
-from rest_framework.views import View
 
 
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj: User) -> bool:
-        if request.method in permissions.SAFE_METHODS:
-            return True
         return request.user.is_authenticated and obj == request.user
 
 
 class IsStaff(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        # if request.method in permissions.SAFE_METHODS:
-        #     return True
+        try:
+            user = User.objects.get(username=request.user)
+        except:
+            return False
+
+        if user.type_user == "Staff":
+            return True
+
+        return False
+
+
+class IsStaffOrSafeMethods(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
 
         try:
             user = User.objects.get(username=request.user)
