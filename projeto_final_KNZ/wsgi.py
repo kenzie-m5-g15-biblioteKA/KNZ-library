03686cac2614186b58c476aa9bc298b88c8dd4e7
Maksuel Nascimento <mkdias_nascimento@hotmail.com>
2023-07-07 17:30:12 -0300

    feat: add scheduler to check lending status

--- projeto_final_KNZ/wsgi.py
+++ projeto_final_KNZ/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_final_KNZ.settings')
+from lendings.scheduler import run_scheduler
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "projeto_final_KNZ.settings")
 
 application = get_wsgi_application()
+
+run_scheduler()
