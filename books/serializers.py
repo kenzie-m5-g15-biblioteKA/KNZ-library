a36458293d85ef9c342f971cdfae96fc50fae507
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:48:20 -0300

    feat:add book serializers

--- books/serializers.py
+++ books/serializers.py
@@ -1,25 +1,32 @@
 from rest_framework import serializers
-from .models import Books
-from users.serializers import UserSerializer
+from users.serializers import UserShortSerializer
+from .models import Book
 
 
-class BooksSerializer(serializers.ModelSerializer):
-    # user = serializers.SerializerMethodField()
-
-    # def get_user(self, data):
-    #     user = data.user
-    #     serializer = UserSerializer(user)
-    #     return serializer.data
-
-    def create(self, validated_data):
-        return Books.objects.create(**validated_data)
+class BookSerializer(serializers.ModelSerializer):
+    followers = UserShortSerializer(read_only=True, many=True)
 
     class Meta:
-        model = Books
+        model = Book
         fields = [
             "id",
-            "name",
+            "title",
+            "author",
             "published_date",
+            "publishing_company",
+            "availability",
+            "ranking",
+            "assessments",
             "followers",
         ]
+        depth = 1
         extra_kwargs = {"followers": {"read_only": True}}
+
+
+class BookShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = [
+            "title",
+            "author",
+        ]
