129ff4818c8aa95b64409a624964e3b61fae558b
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:48:54 -0300

    feat:add book urls

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import BookView, BookDetailView
+from assessments.views import CreateAssessmentsView
+from .views import BookView, BookDetailView, BookFollowView, BookUnfollowView
 
 
 urlpatterns = [
     path("books/", BookView.as_view()),
     path("books/<int:pk>/", BookDetailView.as_view()),
+    path("users/follow/<int:pk>/", BookFollowView.as_view()),
+    path("users/unfollow/<int:pk>/", BookUnfollowView.as_view()),
+    path("books/<int:pk>/assessments/", CreateAssessmentsView.as_view()),
 ]
