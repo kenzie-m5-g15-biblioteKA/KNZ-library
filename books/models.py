f3046d1669a1d993316d3417c018f3044d5ccf23
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:47:29 -0300

    feat:add book model

--- books/models.py
+++ books/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
 
-class Books(models.Model):
+class AvailabilityOptions(models.TextChoices):
+    unavailable = "unavaliable"
+    avaliable = "avaliable"
+
+
+class Book(models.Model):
     class Meta:
         ordering = ["id"]
 
-    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
     published_date = models.DateField()
+    publishing_company = models.CharField(max_length=100)
+
+    availability = models.CharField(
+        max_length=255,
+        choices=AvailabilityOptions.choices,
+        default=AvailabilityOptions.avaliable,
+    )
+
+    ranking = models.DecimalField(
+        default=0,
+        max_digits=10,
+        decimal_places=2,
+    )
 
     followers = models.ManyToManyField(
         "users.User",
         related_name="Books",
-        
     )
