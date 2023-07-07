0c6c2fafb6e2f6e4a3915b5eb7cb86c42f890cfe
Rafael Rocha <Rafaelgot10@outlook.com>
2023-07-07 12:58:22 -0300

    feat:add lending urls

---
+++ lending/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import (
+    LendingView,
+    CreateLendingView,
+    LendingDetailView,
+    DevolutionLendingView,
+    updateLendingView,
+    DeleteLendingView,
+)
+
+
+urlpatterns = [
+    path("lending/", LendingView.as_view()),
+    path("book/<int:pk>/lending/", CreateLendingView.as_view()),
+    path("user/lending/", LendingDetailView.as_view()),
+    path("user/lending/<int:pk>/", DevolutionLendingView.as_view()),
+    path("lending/update/", updateLendingView.as_view()),
+    path("lending/<int:pk>/", DeleteLendingView.as_view()),
+]
